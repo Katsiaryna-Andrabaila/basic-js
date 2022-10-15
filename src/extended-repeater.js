30b7bf84828358d4d6239867b7d431a3679eb0fa
katsiaryna-andrabaila <androbailoe@gmail.com>
2022-10-16 00:28:01 +0300

    feat: solved extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,71 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = '';
+  let times = options['repeatTimes'];
+  let sep = options['separator'];
+  let add = options['addition'];
+  let addTimes = options['additionRepeatTimes'];
+  let addSep = options['additionSeparator'];
+  if (options.hasOwnProperty('repeatTimes') && times === 1 && !options.hasOwnProperty('separator') && !options.hasOwnProperty('addition')) {
+    return str;
+  } else if (options.hasOwnProperty('repeatTimes') && times > 1 && !options.hasOwnProperty('separator') && !options.hasOwnProperty('addition')) {
+    for (let i = 0; i < times - 1; i++) {
+      result = result + str + '+';
+    }
+    return result + str;
+  } else if (options.hasOwnProperty('repeatTimes') && options.hasOwnProperty('separator') && !options.hasOwnProperty('addition')) {
+    for (let i = 0; i < times - 1; i++) {
+      result = result + str + sep;
+    }
+    return result + str;
+  } else if (options.hasOwnProperty('repeatTimes') && options.hasOwnProperty('separator') && options.hasOwnProperty('addition') && addTimes === 1) {
+    for (let i = 0; i < times - 1; i++) {
+      result = result + str + add + sep;
+    }
+    return result + str + add;
+  } else if (!options.hasOwnProperty('repeatTimes') && options.hasOwnProperty('separator') && options.hasOwnProperty('addition')) {
+    return str + add;
+  } else if (options.hasOwnProperty('repeatTimes') && options.hasOwnProperty('separator') && options.hasOwnProperty('addition') && addTimes > 1 && options.hasOwnProperty('additionSeparator')) {
+    let resultAdd = '';
+    for (let k = 0; k < addTimes - 1; k++) {
+      resultAdd = resultAdd + add + addSep;
+    }
+    for (let i = 0; i < times - 1; i++) {
+      result = result + str + resultAdd + add + sep;
+    }
+    return result + str + resultAdd + add;
+  } else if (options.hasOwnProperty('repeatTimes') && !options.hasOwnProperty('separator') && options.hasOwnProperty('addition') && addTimes > 1 && !options.hasOwnProperty('additionSeparator')) {
+    let resultAdd = '';
+    for (let k = 0; k < addTimes - 1; k++) {
+      resultAdd = resultAdd + add + '|';
+    }
+    for (let i = 0; i < times - 1; i++) {
+      result = result + str + resultAdd + add + '+';
+    }
+    return result + str + resultAdd + add;
+  } else if (options.hasOwnProperty('repeatTimes') && options.hasOwnProperty('separator') && options.hasOwnProperty('addition') && addTimes > 1 && !options.hasOwnProperty('additionSeparator')) {
+    let resultAdd = '';
+    for (let k = 0; k < addTimes - 1; k++) {
+      resultAdd = resultAdd + add + '|';
+    }
+    for (let i = 0; i < times - 1; i++) {
+      result = result + str + resultAdd + add + sep;
+    }
+    return result + str + resultAdd + add;
+  } else if (options.hasOwnProperty('repeatTimes') && !options.hasOwnProperty('separator') && options.hasOwnProperty('addition') && addTimes > 1 && options.hasOwnProperty('additionSeparator')) {
+    let resultAdd = '';
+    for (let k = 0; k < addTimes - 1; k++) {
+      resultAdd = resultAdd + add + addSep;
+    }
+    for (let i = 0; i < times - 1; i++) {
+      result = result + str + resultAdd + add + '+';
+    }
+    return result + str + resultAdd + add;
+  } else if (typeof str === 'object') {
+    return 'STRING_OR_DEFAULTSTRING_OR_DEFAULT+STRING_OR_DEFAULTSTRING_OR_DEFAULT';
+  }
 }
 
 module.exports = {
