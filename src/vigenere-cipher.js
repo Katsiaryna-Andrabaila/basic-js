d49ffafdce66bb96bfab8636dfc35c6581292c56
katsiaryna-andrabaila <androbailoe@gmail.com>
2022-10-16 17:41:12 +0300

    feat: solved vigenere-cipher.test.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -21,32 +21,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
  
 class VigenereCipheringMachine {
+  constructor(reversed = true) {
+    this.reversed = reversed;
+  }
   encrypt(str, key) {
     if (!str || !key || str === undefined) {
       throw new Error ('Incorrect arguments!');
     }
+    if (!this.reversed) {
+      str = str.split('').reverse().join('');
+    }
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let strUpper = str.toUpperCase();
-    let keyUpper = key.padEnd(str.length, key).toUpperCase();
+    let strUpper = str.toUpperCase().replace(/ /g,'');
+    let keyUpper = key.padEnd(strUpper.length, key).toUpperCase();
     let result = '';
     for (let i = 0, j = 0; i < strUpper.length; i++, j++) {
-      let keyUpperWithSpaces = keyUpper.slice(0, i + 1);
       if (!letters.includes(strUpper[i])) {
         result = result + strUpper[i];
-        
         continue;
       }
       result = result + letters[(letters.indexOf(strUpper[i]) + letters.indexOf(keyUpper[j])) % 26];
       if (j === keyUpper.length - 1) j = -1;
     }
+    let arr = str.split('');
+    let resultArr = result.split('');
+    for (let i = 0; i < str.length; i++) {
+      if (arr[i] === ' ') {
+        resultArr.splice(i, 0, ' ');
+      }
+    }
     
-    return result;
+    return resultArr.join('');
   }
   decrypt(str, key) {
     if (!str || !key || str === undefined) {
       throw new Error ('Incorrect arguments!');
     }
+    if (!this.reversed) {
+      str = str.split('').reverse().join('');
+    }
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let strUpper = str.toUpperCase().replace(/ /g,'');
+    let keyUpper = key.padEnd(strUpper.length, key).toUpperCase();
+    let result = '';
+    for (let i = 0, j = 0; i < strUpper.length; i++, j++) {
+      if (!letters.includes(strUpper[i])) {
+        result = result + strUpper[i];
+        continue;
+      }
+      if (letters.indexOf(strUpper[i]) - letters.indexOf(keyUpper[j]) < 0) {
+        result = result + letters[(letters.indexOf(strUpper[i]) - letters.indexOf(keyUpper[j])) + 26];
+      } else  {
+        result = result + letters[(letters.indexOf(strUpper[i]) - letters.indexOf(keyUpper[j])) % 26];
+      }
+      if (j === keyUpper.length - 1) j = -1;
+    }
+    let arr = str.split('');
+    let resultArr = result.split('');
+    for (let i = 0; i < str.length; i++) {
+      if (arr[i] === ' ') {
+        resultArr.splice(i, 0, ' ');
+      }
+    }
     
+    return resultArr.join('');
   }
 }
 
