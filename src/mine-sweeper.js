9381fd9f1f20a4cd9b471820c4dbd719d578ac77
katsiaryna-andrabaila <androbailoe@gmail.com>
2022-10-16 15:58:44 +0300

    feat: passed some tests

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = [];
+  let flatArr = matrix.flat();
+  if (flatArr.every(el => el === false)) {
+    result.push(flatArr.slice(0, flatArr.length / matrix.length));
+    result.push(flatArr.slice(flatArr.length / matrix.length));
+    return result;
+  } else {
+    
+    //let zeroArr = flatArr.forEach(el => el === 0);
+    for (let i = 0; i < matrix.length - 1; i++) {
+      for (let k = 0; k < 3; k++) {
+        let count = 0;
+        if (matrix[i][k - 1] && matrix[i][k - 1] === true) count++;
+        if (matrix[i][k + 1] && matrix[i][k + 1] === true) count++;
+        if (matrix[i + 1][k] && matrix[i + 1][k] === true) count++;
+        if (matrix[i + 1][k - 1] && matrix[i + 1][k - 1] === true) count++;
+        if (matrix[i + 1][k + 1] && matrix[i + 1][k + 1] === true) count++;
+        //if (matrix[i - 1][k] && matrix[i - 1][k] === true) count++;
+        //if (matrix[i - 2][k - 1] && matrix[i - 2][k - 1] === true) count++;
+        //if (matrix[i - 2][k + 1] && matrix[i - 2][k + 1] === true) count++;
+        matrix[i][k] = count;
+      }
+    }
+    return matrix;
+  }
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -6,9 +6,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * @example
  * 
- * const directMachine = new VigenereCipheringMachine();
+ const directMachine = new VigenereCipheringMachine();
  * 
- * const reverseMachine = new VigenereCipheringMachine(false);
+ const reverseMachine = new VigenereCipheringMachine(false);
  * 
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
  * 
@@ -19,16 +19,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+ 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(str, key) {
+    if (!str || !key || str === undefined) {
+      throw new Error ('Incorrect arguments!');
+    }
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let strUpper = str.toUpperCase();
+    let keyUpper = key.padEnd(str.length, key).toUpperCase();
+    let result = '';
+    for (let i = 0, j = 0; i < strUpper.length; i++, j++) {
+      let keyUpperWithSpaces = keyUpper.slice(0, i + 1);
+      if (!letters.includes(strUpper[i])) {
+        result = result + strUpper[i];
+        
+        continue;
+      }
+      result = result + letters[(letters.indexOf(strUpper[i]) + letters.indexOf(keyUpper[j])) % 26];
+      if (j === keyUpper.length - 1) j = -1;
+    }
+    
+    return result;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str, key) {
+    if (!str || !key || str === undefined) {
+      throw new Error ('Incorrect arguments!');
+    }
+    
   }
 }
+
+const directMachine = new VigenereCipheringMachine();
+const reverseMachine = new VigenereCipheringMachine(false);
 
 module.exports = {
   VigenereCipheringMachine
